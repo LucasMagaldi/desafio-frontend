a44f0d556c9492f679be6c4ee46ef91b09d08d03
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-07-13 13:03:17 -0300

    [updated] list of channels

--- youtube_bycoders/src/components/subscription/Subscription.jsx
+++ youtube_bycoders/src/components/subscription/Subscription.jsx
@@ -8,21 +8,32 @@ import './subscription.css'
 const Subscription = ({subscriptions, authenticate}) => {
   
   return (
-    <div className='container_auth_failed'>
+    <>
       {
         authenticate ?
-        <div className='failed_section'>
-          <img src={failedAuth} alt="" />
-          <h7>To view your subscriptions You must be Authenticate</h7>
+        <div className='container_auth_failed'>
+          <div className='failed_section'>
+            <img src={failedAuth} alt="" />
+            <h7>To view your subscriptions You must be Authenticate</h7>
+          </div>  
         </div>    
         :
 
-        <div className=''>
-            <h1>channels</h1>
+        <div className='channels_section'>
+            {subscriptions.map((item, key) => (
+              <div className='channels_item'>
+                  <img src={item.snippet.thumbnails.medium.url} />
+                  <div className='channel_info'>
+                    <h2>{item.snippet.title}</h2>
+                    <h4>{item.snippet.channelTitle}</h4>
+                    <p>{item.snippet.description}</p>
+                  </div>
+              </div>
+            ))}
         </div>
       }
-        
-    </div>
+    </>    
+    
   )
 }
 
